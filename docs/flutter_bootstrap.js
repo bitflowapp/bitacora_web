4f819a4b73e1cd6d6490a1b762faddc8d454164a
gridnote <marcoantoniolunavillegas@gmail.com>
2025-12-10 05:34:27 -0300

    Deploy BitFlow web beta a GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1924652396"
+    serviceWorkerVersion: "1396919053"
   }
 });
