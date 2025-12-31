f8672e3041d6c9cd1d03abc8b7ff83a419a06a32
gridnote <marcoantoniolunavillegas@gmail.com>
2025-12-31 01:34:23 -0300

    Deploy web (release) 20251231-013423

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "166bdf9a1ceac23d8c4bd63185572848
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "28638efb5162eee555ec792f10fbf0a1",
+"flutter_bootstrap.js": "e9324dfa076da2718bfca16f11aed598",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
