5c4bd42fa41bdabf1b7827e91faa635fcd528b38
Marco Luna <marcoantoniolunavillegas@gmail.com>
2026-03-11 00:03:16 -0300

    deploy: Flutter web (v1.3.1+11 full build)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2868251026"
+    serviceWorkerVersion: "2936711239"
   }
 });
