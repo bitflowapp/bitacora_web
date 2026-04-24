c942e401d3a858c70fed01791eb276d4fa2947c4
gridnote <marcoantoniolunavillegas@gmail.com>
2026-04-24 04:28:58 -0300

    feat(android): prepare offline Android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ fun signingProp(name: String): String {
 }
 
 android {
-    namespace = "com.example.bitacora_web"
+    namespace = "com.bitflow.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -39,8 +39,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bitacora_web"
+        applicationId = "com.bitflow.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
